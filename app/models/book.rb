67a7548c3f1561b0934ec1a3422c4026e99f0bc7
Solacx <junyong6@gmail.com>
2020-09-28 15:32:52 +1000

    Address issues in pull request.

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,13 +4,7 @@ class Book < ApplicationRecord
   has_one_attached :content
 
   validates_presence_of :title, :author, :reference_number, :book_type
-  validates :cover_image,
-            attached: true
-            # dimension: {
-            #   width: { min: 100, max: 300 },
-            #   height: { min: 100, max: 300 },
-            #   message: 'is not given between dimension. Height between 100 and 300, width between 100 and 300.'
-            # }
+  validates :cover_image, attached: true
   validates :cover_image, content_type: /\Aimage\/.*\z/
   validates :content, attached: true, content_type: { in: 'application/pdf', message: 'is not a PDF' }
 
