d2d2781ee8a93a2288265dbb78aeef0cb8d4552e
Davit Gevorgyan <dgev2k@gmail.com>
2020-10-18 19:50:32 +1100

    Fixed Schema version issue

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_17_202846) do
+ActiveRecord::Schema.define(version: 2020_10_17_053710) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
