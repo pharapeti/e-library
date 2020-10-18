35b518cbf027aab321187ba67fb6444e4eb5f42a
pharapeti <patrice.harapeti@gmail.com>
2020-10-18 18:11:42 +1100

    More ui changes and better login

--- spec/features/library_managers/library_manager_views_loan_history_spec.rb
+++ spec/features/library_managers/library_manager_views_loan_history_spec.rb
@@ -21,10 +21,10 @@ RSpec.describe 'Library Manager views loan history', type: :feature, js: true do
       find('tr', text: 'How to code').click_link('Show')
     end
 
-    # Check Loan History 
+    # Check Loan History
     expect(page).to have_current_path book_path(books(:book_3))
     expect(page).to have_text 'Loan History'
-    expect(page).to have_text 'User ID'
+    expect(page).to have_text 'Borrower'
     expect(page).to have_text 'Borrow date'
     expect(page).to have_text 'Return date'
     expect(page).to have_text 'Late return'
